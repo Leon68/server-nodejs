405a3a610355be0e9994014bf3dd291819b14b0f
Leon <wanliangwang@163.com>
2017-09-30 10:18:17 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '47.91.156.35';
+const port = 6868;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+
+});
+
